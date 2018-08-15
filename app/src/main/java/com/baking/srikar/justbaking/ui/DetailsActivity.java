d6d149c9141970dc1c794b5078e867047a16ac9d
Srikar <srikarnanduri628@gmail.com>
2018-08-15 17:18:36 +0530

    Fixes

--- app/src/main/java/com/baking/srikar/justbaking/ui/DetailsActivity.java
+++ app/src/main/java/com/baking/srikar/justbaking/ui/DetailsActivity.java
@@ -1,5 +1,6 @@
 package com.baking.srikar.justbaking.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,9 @@ public class DetailsActivity extends AppCompatActivity {
         bundle.putString("bakinglist", bakinglist);
         RecipeDetailsListFragment fragment = new RecipeDetailsListFragment();
         fragment.setArguments(bundle);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.recipe_details_fragment_body_part, fragment)
+                .commit();
        /* Gson gson = new Gson();
         BakingResponse bakingResponse = gson.fromJson(bakinglist, BakingResponse.class);
         Log.v("Json Data", bakingResponse.getName());*/
--- app/src/main/java/com/baking/srikar/justbaking/ui/RecipeDetailsListFragment.java
+++ app/src/main/java/com/baking/srikar/justbaking/ui/RecipeDetailsListFragment.java
@@ -9,12 +9,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.baking.srikar.justbaking.Models.BakingResponse;
 import com.baking.srikar.justbaking.R;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
 public class RecipeDetailsListFragment extends Fragment {
+
+    @BindView(R.id.ingredients_tv)
+    TextView ingredientsTv;
+
+    @BindView(R.id.recipe_steps_rv)
+    RecyclerView stepsRv;
+
     public RecipeDetailsListFragment() {
     }
 
@@ -26,7 +39,28 @@ public class RecipeDetailsListFragment extends Fragment {
         Gson gson = new Gson();
         BakingResponse bakingResponse = gson.fromJson(bakingList, BakingResponse.class);
         Log.v("Json Data", bakingResponse.getName());
-        
+        ingredients(bakingResponse);
         return rootView;
     }
+
+    public void ingredients(BakingResponse bakingResponse){
+        List<String> ingridentList =new ArrayList<String>();
+        for(int i=0; i<bakingResponse.getIngredients().size(); i++){
+            String ingridents = bakingResponse.getIngredients().get(i).getIngredient();
+            double quantityValue = bakingResponse.getIngredients().get(i).getQuantity();
+            String measure = bakingResponse.getIngredients().get(i).getMeasure() ;
+            String ingridentsListString;
+            if ((quantityValue == Math.floor(quantityValue)) && !Double.isInfinite(quantityValue)) {
+                // integer type
+                ingridentsListString = ingridents + "(" + (int)quantityValue + " " + measure + ")";
+            } else {
+                ingridentsListString = ingridents + "(" +  quantityValue + " " + measure + ")";
+            }
+
+            ingridentList.add(ingridentsListString);
+        }
+        Log.v("IngridentsList(0)", ingridentList.get(1) );
+
+    }
+
 }
