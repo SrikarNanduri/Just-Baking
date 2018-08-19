9958da3f3c6d769f9172383c11bc9ee902348994
Srikar <srikarnanduri628@gmail.com>
2018-08-19 19:01:00 +0530

    Added Steps details Fragment and ui for it.

--- app/src/main/java/com/baking/srikar/justbaking/Config/ConfigURL.java
+++ app/src/main/java/com/baking/srikar/justbaking/Config/ConfigURL.java
@@ -2,5 +2,4 @@ package com.baking.srikar.justbaking.Config;
 
 public class ConfigURL {
     public static final String BaseURL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
-
 }
