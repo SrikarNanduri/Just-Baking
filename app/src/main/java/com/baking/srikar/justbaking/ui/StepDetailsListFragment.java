e48c2166380db75059d097056915073365363715
Srikar <srikarnanduri628@gmail.com>
2018-08-19 20:12:46 +0530

    Added exo player need to improve it.

--- app/src/main/java/com/baking/srikar/justbaking/ui/StepDetailsListFragment.java
+++ app/src/main/java/com/baking/srikar/justbaking/ui/StepDetailsListFragment.java
@@ -1,5 +1,6 @@
 package com.baking.srikar.justbaking.ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.TextView;
 
 import com.baking.srikar.justbaking.Models.Step;
 import com.baking.srikar.justbaking.R;
 
-import java.util.List;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +36,13 @@ public class StepDetailsListFragment extends Fragment{
 
     @BindView(R.id.stepDetail_tv)
     TextView stepsTv;
+
+    @BindView(R.id.simpleExoPlayerView)
+     SimpleExoPlayerView simpleExoPlayerView;
+
+    private SimpleExoPlayer mExoPlayer;
+    private BandwidthMeter bandwidthMeter;
+
 
     public StepDetailsListFragment() {
     }
@@ -32,6 +54,40 @@ public class StepDetailsListFragment extends Fragment{
         ButterKnife.bind(this, rootView);
         Step steps = getArguments().getParcelable("Steps");
         stepsTv.setText(steps.getDescription());
+        exoPlayer(steps);
         return rootView;
+    }
+
+    public void exoPlayer(Step steps) {
+        simpleExoPlayerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+        String videoURL = steps.getVideoURL();
+        initializePlayer(Uri.parse(videoURL));
+    }
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            simpleExoPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 }
