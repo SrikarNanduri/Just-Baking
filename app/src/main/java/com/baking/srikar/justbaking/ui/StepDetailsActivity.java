7ce6e5021d989670b2f190aca674aab155917941
srikarn <srikarnanduri628@gmail.com>
2018-08-22 16:50:45 +0530

    UI fixes

--- app/src/main/java/com/baking/srikar/justbaking/ui/StepDetailsActivity.java
+++ app/src/main/java/com/baking/srikar/justbaking/ui/StepDetailsActivity.java
@@ -17,20 +17,12 @@ import butterknife.ButterKnife;
 
 public class StepDetailsActivity extends AppCompatActivity {
 
-    @BindView(R.id.my_toolbar)
-    Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_details);
-
-        ButterKnife.bind(this);
-
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("Baking Time");
 
         String steps = getIntent().getStringExtra("stepsList");
         Log.v("StepsData", steps);
